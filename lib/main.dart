8196f3fd103b8186aec2b7607059bd1b841103d8
Daniel Ramos <ramosdarc-18@outloo.com>
2022-11-10 14:28:55 +0100

    creando clase separada para el Body

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ChatApp',
-      home: Scaffold(
+        debugShowCheckedModeBanner: false,
+        title: 'ChatApp',
+        home: Scaffold(
           appBar: AppBar(
             centerTitle: false,
             title: const Text('Hermod Messenger'),
           ),
-          body: Center(
-            child: Column(children: <Widget>[
-              Image.asset(
-                'assets/images/prueba.jpeg',
-                height: 400,
-                width: 250,
-                alignment: Alignment.centerRight,
-              ),
-            ]),
-          )),
-    );
+          body: const CustomBody(),
+        ));
+  }
+}
+
+class CustomBody extends StatelessWidget {
+  const CustomBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Image.asset(
+        'assets/images/prueba.jpeg',
+        height: 400,
+        width: 250,
+        alignment: Alignment.centerRight,
+      ),
+      TextFormField(
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'Enter your email',
+          labelText: 'Email *',
+        ),
+        validator: (String? value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter some text';
+          }
+          return null;
+        },
+      )
+    ]);
   }
 }
